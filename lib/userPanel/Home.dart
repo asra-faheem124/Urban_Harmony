a70fd830a6a05cfc90d4528bbdd03186dbe61be4
Syeda Nabiha <159924237+Syeda-Nabihaa@users.noreply.github.com>
2025-06-20 22:15:41 +0500

    Added carousel_slider dependency and updated main.dart to use HomeScreen

---
+++ lib/userPanel/Home.dart
@@ -0,0 +1,182 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 50,
+            width: 100,
+            child: Image.asset('assets/images/logo2.png'),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications, color: Colors.black),
+            onPressed: () {},
+          ),
+          Builder(
+            builder:
+                (context) => IconButton(
+                  icon: Icon(Icons.menu, color: Colors.black),
+                  onPressed: () {
+                    // Open the right-side drawer
+                    Scaffold.of(context).openEndDrawer();
+                  },
+                ),
+          ),
+        ],
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo.png"),
+                ),
+              ),
+              child: Text("sksk"),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Contact'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Wish List'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user),
+              title: Text('User Account'),
+              onTap: () {},
+            ),
+            // Add more list items here
+          ],
+        ),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 8.0,
+              ),
+              child: Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width: 450,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 7.0,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Try Search here...",
+
+                      suffixIcon: Icon(Icons.search),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 16 / 9,
+                autoPlayInterval: Duration(seconds: 3),
+                viewportFraction: 0.9,
+              ),
+              items:
+                  [
+                    'assets/images/slider1.jpg',
+                    'assets/images/slider2.jpg',
+                    'assets/images/slider3.jpg',
+                  ].map((imagePath) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            imagePath,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(26.0),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Categories",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton.icon(
+                      style: TextButton.styleFrom(iconColor: Colors.black),
+                      onPressed: () {},
+                      label: Text(
+                        "See more",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      icon: Icon(Icons.arrow_forward),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
